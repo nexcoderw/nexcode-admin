04b2e4b1db66437ac76bf6fe81f5f0fd2a083a70
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-09-22 03:51:08 +0200

    Compose the sign-in page with the interactive login form

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,16 +1,6 @@
-import {
-  ArrowRight01Icon,
-  LockPasswordIcon,
-  Mail01Icon,
-} from "@hugeicons/core-free-icons";
 import type { Metadata } from "next";
-import Link from "next/link";
 
-import { Button } from "../../../components/ui/Button/Button";
-import { Checkbox } from "../../../components/ui/Checkbox/Checkbox";
-import { Icon } from "../../../components/ui/Icon/Icon";
-import { Input } from "../../../components/ui/Input/Input";
-import { PasswordInput } from "../../../components/ui/PasswordInput/PasswordInput";
+import { LoginForm } from "@/components/auth/LoginForm/LoginForm";
 
 import styles from "./page.module.css";
 
@@ -33,47 +23,7 @@ export default function LoginPage() {
         </p>
       </header>
 
-      <form className={styles.form} action="#">
-        <div className={styles.fields}>
-          <Input
-            type="email"
-            name="email"
-            label="Email address"
-            placeholder="Enter your email address"
-            autoComplete="email"
-            inputMode="email"
-            leftIcon={<Icon icon={Mail01Icon} size={18} />}
-            required
-          />
-
-          <PasswordInput
-            name="password"
-            label="Password"
-            placeholder="Enter your password"
-            autoComplete="current-password"
-            leftIcon={<Icon icon={LockPasswordIcon} size={18} />}
-            required
-          />
-        </div>
-
-        <div className={styles.options}>
-          <Checkbox name="remember" label="Remember me" />
-
-          <Link href="/forgot-password" className={styles.forgotPassword}>
-            Forgot password?
-          </Link>
-        </div>
-
-        <div className={styles.submit}>
-          <Button
-            type="submit"
-            size="lg"
-            rightIcon={<Icon icon={ArrowRight01Icon} size={18} />}
-          >
-            Sign in
-          </Button>
-        </div>
-      </form>
+      <LoginForm />
     </div>
   );
 }
