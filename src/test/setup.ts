7b81202d691527c4bde110bb35e3b6c6e30e3067
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-09-22 15:02:09 +0200

    Initialize DOM assertions and test cleanup

---
+++ src/test/setup.ts
@@ -0,0 +1,15 @@
+import "@testing-library/jest-dom/vitest";
+
+import {
+    cleanup,
+} from "@testing-library/react";
+import {
+    afterEach,
+    vi,
+} from "vitest";
+
+afterEach(() => {
+    cleanup();
+
+    vi.unstubAllGlobals();
+});
