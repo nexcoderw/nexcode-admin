12cc33b27a518bb339471542b86c09b66dabd284
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-09-22 18:24:29 +0200

    Resolve Team images between local media and Cloudinary storage

---
+++ src/utils/team/team-image-source.ts
@@ -0,0 +1,36 @@
+import type {
+    NextConfig,
+} from "next";
+
+const nextConfig: NextConfig = {
+    reactCompiler: true,
+
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname:
+                    "res.cloudinary.com",
+            },
+        ],
+    },
+
+    async headers() {
+        return [
+            {
+                source: "/:path*",
+
+                headers: [
+                    {
+                        key:
+                            "X-Robots-Tag",
+                        value:
+                            "noindex, nofollow, noarchive",
+                    },
+                ],
+            },
+        ];
+    },
+};
+
+export default nextConfig;
