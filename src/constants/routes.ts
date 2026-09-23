c01031e6f7a119f7214cc84cc9548288e42bb97b
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-09-23 18:19:23 +0200

    Register independent Portfolio BFF routes

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -54,4 +54,74 @@ export const API_ROUTES = {
 
     media: "/api/team/media",
   },
+  portfolio: {
+    list:
+      "/api/portfolio/list",
+
+    add:
+      "/api/portfolio/add",
+
+    detail: (
+      portfolioId: number,
+    ) =>
+      `/api/portfolio/detail/${portfolioId}`,
+
+    update: (
+      portfolioId: number,
+    ) =>
+      `/api/portfolio/update/${portfolioId}`,
+
+    delete: (
+      portfolioId: number,
+    ) =>
+      `/api/portfolio/delete/${portfolioId}`,
+
+    imageAdd: (
+      portfolioId: number,
+    ) =>
+      `/api/portfolio/image/add/${portfolioId}`,
+
+    imageUpdate: (
+      imageId: number,
+    ) =>
+      `/api/portfolio/image/update/${imageId}`,
+
+    imageDelete: (
+      imageId: number,
+    ) =>
+      `/api/portfolio/image/delete/${imageId}`,
+
+    documentAdd: (
+      portfolioId: number,
+    ) =>
+      `/api/portfolio/document/add/${portfolioId}`,
+
+    documentUpdate: (
+      documentId: number,
+    ) =>
+      `/api/portfolio/document/update/${documentId}`,
+
+    documentDelete: (
+      documentId: number,
+    ) =>
+      `/api/portfolio/document/delete/${documentId}`,
+
+    repositoryAdd: (
+      portfolioId: number,
+    ) =>
+      `/api/portfolio/repository/add/${portfolioId}`,
+
+    repositoryUpdate: (
+      repositoryId: number,
+    ) =>
+      `/api/portfolio/repository/update/${repositoryId}`,
+
+    repositoryDelete: (
+      repositoryId: number,
+    ) =>
+      `/api/portfolio/repository/delete/${repositoryId}`,
+
+    media:
+      "/api/portfolio/media",
+  },
 } as const;
