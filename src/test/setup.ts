cd5833a8b6793ac6676aee68d19222f92a583369
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-09-23 20:56:04 +0200

    Provide dialog behaviour for browser component tests

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -8,6 +8,28 @@ import {
     vi,
 } from "vitest";
 
+if (
+    typeof HTMLDialogElement !==
+    "undefined"
+) {
+    HTMLDialogElement.prototype
+        .showModal =
+        function showModal() {
+            this.setAttribute(
+                "open",
+                "",
+            );
+        };
+
+    HTMLDialogElement.prototype
+        .close =
+        function close() {
+            this.removeAttribute(
+                "open",
+            );
+        };
+}
+
 afterEach(() => {
     cleanup();
 
