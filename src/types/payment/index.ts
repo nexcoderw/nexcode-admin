658a991dd197b3dd71d52d49cfb5c75e07e04b6c
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-09-24 22:51:37 +0200

    feat(payment): export payment domain types

---
+++ src/types/payment/index.ts
@@ -0,0 +1,7 @@
+export * from "@/types/payment/agreement";
+export * from "@/types/payment/installment";
+export * from "@/types/payment/record";
+export * from "@/types/payment/reminder";
+export * from "@/types/payment/schedule";
+export * from "@/types/payment/shared";
+export * from "@/types/payment/summary";
